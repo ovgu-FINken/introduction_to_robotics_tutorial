6e5deed2ca73cd4849e8944fba10919d7dae8f5e
Sebastian Mai <sebastian.mai@ovgu.de>
2022-04-19 11:12:45 +0200

    assignment 2

---
+++ src/reactive_behaviour/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'reactive_behaviour'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='basti',
+    maintainer_email='sebastian.mai@ovgu.de',
+    description='package for reactive behaviour tutorial',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'controller = reactive_behaviour.controller:main'
+        ],
+    },
+)
