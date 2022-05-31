dcdd7896b0ddb5ac7d8fdbd69caf84b705b947db
Sebastian Mai <sebastian.mai@ovgu.de>
2022-05-31 12:27:06 +0200

    benchmarks

--- src/state_estimation/launch/benchmark1.launch.py
+++ src/state_estimation/launch/benchmark1.launch.py
@@ -38,14 +38,14 @@ def controller_spawning(context, *args, **kwargs):
                 'use_sim_time': use_sim_time,
                 'rate' : 0.1,
                 'anchor_list': '''
-- {x: 0.0, y: 0.0, z: 1.0, mu: 0.1, sigma: 0.1}
-- {x: 3.0, y: 3.0, z: 1.0, mu: 0.1, sigma: 0.1}
-- {x: 0.0, y: 3.0, z: 1.0, mu: 0.1, sigma: 0.1}
-- {x: 3.0, y: 0.0, z: 1.0, mu: 0.1, sigma: 0.1}
-- {x: 0.0, y: 0.0, z: 2.0, mu: 0.1, sigma: 0.1}
-- {x: 3.0, y: 3.0, z: 2.0, mu: 0.1, sigma: 0.1}
-- {x: 0.0, y: 3.0, z: 2.0, mu: 0.1, sigma: 0.1}
-- {x: 3.0, y: 0.0, z: 2.0, mu: 0.1, sigma: 0.1}
+- {x: 0.0, y: 0.0, z: 1.0, mu: 1.1, sigma: 0.1}
+- {x: 3.0, y: 3.0, z: 1.0, mu: 1.1, sigma: 0.1}
+- {x: 0.0, y: 3.0, z: 1.0, mu: 1.1, sigma: 0.1}
+- {x: 3.0, y: 0.0, z: 1.0, mu: 1.1, sigma: 0.1}
+- {x: 0.0, y: 0.0, z: 2.0, mu: 1.1, sigma: 0.1}
+- {x: 3.0, y: 3.0, z: 2.0, mu: 1.1, sigma: 0.1}
+- {x: 0.0, y: 3.0, z: 2.0, mu: 1.1, sigma: 0.1}
+- {x: 3.0, y: 0.0, z: 2.0, mu: 1.1, sigma: 0.1}
 ''' 
             }],
             output='screen',
